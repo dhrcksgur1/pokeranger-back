4c441c32fd32c3faa785db7214795426a3a43914
dbdudann <dbdudann@gmail.com>
2024-03-06 18:18:40 +0900

    fix: order에 BaseEntity 적용

--- src/main/java/io/elice/pokeranger/order/entity/Orders.java
+++ src/main/java/io/elice/pokeranger/order/entity/Orders.java
@@ -1,5 +1,6 @@
 package io.elice.pokeranger.order.entity;
 
+import io.elice.pokeranger.global.entity.BaseEntity;
 import io.elice.pokeranger.order.deliverystate.DeliveryStateRole;
 import io.elice.pokeranger.orderItem.entity.OrderItem;
 import io.elice.pokeranger.user.entity.User;
@@ -20,7 +21,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Orders {
+public class Orders extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,9 +32,6 @@ public class Orders {
 
     @Enumerated(EnumType.STRING)
     private DeliveryStateRole deliveryState;
-
-    @CreatedDate
-    private LocalDateTime orderDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
