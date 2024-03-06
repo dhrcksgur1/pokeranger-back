51903ea28061a2bbdbaa060d6645afc4403f79ea
[Cloud]황소현 <babystep9696@gmail.com>
2024-03-07 01:05:19 +0900

    요청 모두 허용

--- src/main/java/io/elice/pokeranger/global/config/CorsConfig.java
+++ src/main/java/io/elice/pokeranger/global/config/CorsConfig.java
@@ -1,16 +1,6 @@
 package io.elice.pokeranger.global.config;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -26,11 +16,12 @@ public class CorsConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("http://kdt-cloud-1-team03.elicecoding.com");
-        config.addAllowedOriginPattern("http://192.168.0.4:5500/");
-        config.addAllowedOriginPattern("http://localhost:8080" ); // 수정된 부분
-        config.addAllowedOriginPattern("http://localhost:63342");
-        config.addAllowedOriginPattern("http://127.0.0.1:5500");
+        config.addAllowedOriginPattern("*");
+//        config.addAllowedOriginPattern("http://kdt-cloud-1-team03.elicecoding.com");
+//        config.addAllowedOriginPattern("http://192.168.0.4:5500/");
+//        config.addAllowedOriginPattern("http://localhost:8080" ); // 수정된 부분
+//        config.addAllowedOriginPattern("http://localhost:63342");
+//        config.addAllowedOriginPattern("http://127.0.0.1:5500");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
