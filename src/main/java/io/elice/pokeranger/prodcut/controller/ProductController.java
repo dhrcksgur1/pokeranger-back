46a3513c422e51beff303521d3fd4e744a5a3339
ChanHyeok <dhrcksgur1@gmail.com>
2024-03-06 18:43:21 +0900

    validation

--- src/main/java/io/elice/pokeranger/prodcut/controller/ProductController.java
+++ src/main/java/io/elice/pokeranger/prodcut/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,15 +31,54 @@ public class ProductController {
     }
 
     //CREATE
-
     @Operation(summary = "물품 등록기능", description = "물품 등록")
     @PostMapping
-    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequestDTO) {
+    public ResponseEntity<ProductResponseDTO> createProduct(@Validated @RequestBody ProductRequestDTO productRequestDTO) {
         ProductResponseDTO createdProduct = productService.createProduct(productRequestDTO);
         return ResponseEntity.ok(createdProduct);
     }
 
-//    //Read All Products
+
+    // Read All Products
+    @GetMapping
+    public ResponseEntity<Page<ProductResponseDTO>> getProductList(@PageableDefault(size = 10) Pageable pageable) {
+        Page<ProductResponseDTO> products = productService.findAllProducts(pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    // Read By UserID
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Page<ProductResponseDTO>> getProductByUserId(@PathVariable Long userId, @PageableDefault(size = 10) Pageable pageable) {
+        Page<ProductResponseDTO> products = productService.getProductsByUserId(userId, pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    // READ BY CategoryID
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<Page<ProductResponseDTO>> getProductByCategoryId(@PathVariable Long categoryId, @PageableDefault(size = 10) Pageable pageable) {
+        Page<ProductResponseDTO> products = productService.getProductsByCategoryId(categoryId, pageable);
+        return ResponseEntity.ok(products);
+    }
+
+    //UPADTE
+    @Operation(summary = "등록 물품 수정 기능", description = "등록된 물품 정보 수정")
+    @PutMapping("/{id}") //수정코드
+    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable Long id,@Validated @RequestBody ProductRequestDTO productRequestDTO) {
+        ProductResponseDTO updatedProduct = productService.updateProduct(id, productRequestDTO);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    //Delete
+    @Operation(summary = "등록 물품 삭제 기능", description = "등록된 물품을 물품고유id로 삭제")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProductById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    //기존 코드들
+
+    //    //Read All Products
 //    @Operation(summary = "물품 조회 기능", description = "전체 물품 조회")
 //    @GetMapping//수정코드
 //    public ResponseEntity<List<ProductResponseDTO>> getProductList() {
@@ -72,46 +112,6 @@ public class ProductController {
 //        return ResponseEntity.ok(products);
 //    }
 
-
-
-    // Read All Products with Pagination
-    @GetMapping
-    public ResponseEntity<Page<ProductResponseDTO>> getProductList(@PageableDefault(size = 10) Pageable pageable) {
-        Page<ProductResponseDTO> products = productService.findAllProducts(pageable);
-        return new ResponseEntity<>(products, HttpStatus.OK);
-    }
-
-    // Read By UserID with Pagination
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<Page<ProductResponseDTO>> getProductByUserId(@PathVariable Long userId, @PageableDefault(size = 10) Pageable pageable) {
-        Page<ProductResponseDTO> products = productService.getProductsByUserId(userId, pageable);
-        return new ResponseEntity<>(products, HttpStatus.OK);
-    }
-
-    // READ BY CategoryID with Pagination
-    @GetMapping("/category/{categoryId}")
-    public ResponseEntity<Page<ProductResponseDTO>> getProductByCategoryId(@PathVariable Long categoryId, @PageableDefault(size = 10) Pageable pageable) {
-        Page<ProductResponseDTO> products = productService.getProductsByCategoryId(categoryId, pageable);
-        return ResponseEntity.ok(products);
-    }
-
-    //UPADTE
-    @Operation(summary = "등록 물품 수정 기능", description = "등록된 물품 정보 수정")
-    @PutMapping("/{id}") //수정코드
-    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable Long id, @RequestBody ProductRequestDTO productRequestDTO) {
-        ProductResponseDTO updatedProduct = productService.updateProduct(id, productRequestDTO);
-        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
-    }
-
-    //Delete
-    @Operation(summary = "등록 물품 삭제 기능", description = "등록된 물품을 물품고유id로 삭제")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.deleteProductById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    //기존 코드들
     //    @GetMapping//기존코드
 //    public List<Product> getProductList(){
 //        return productService.findAllProducts();
--- src/main/java/io/elice/pokeranger/prodcut/entity/ProductRequestDTO.java
+++ src/main/java/io/elice/pokeranger/prodcut/entity/ProductRequestDTO.java
@@ -1,6 +1,9 @@
 package io.elice.pokeranger.prodcut.entity;
 
 import io.elice.pokeranger.orderItem.entity.CartItemDTO;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,11 +13,25 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class ProductRequestDTO {
+    @NotNull(message = "User ID 는 필수 입력란 입니다.")
     private Long userId;
+
+    @NotNull(message = "Category ID 는 필수 입력란 입니다.")
     private Long categoryId;
+
+    @NotBlank(message = "제품 명은 필수 입력란 입니다.")
     private String name;
+
+    @NotNull(message = "Price 는 필수 입력란 입니다")
+    @Min(value = 0, message = "0이상의 가격을 입력해 주세요.")
     private Long price;
+
+    @NotNull(message = "재고는 필수 입력란 입니다")
+    @Min(value = 1, message = "1개 이상의 재고을 입력해 주세요.")
     private Long stock;
+
+    @NotNull(message = "상세설명은 필수 입력란 입니다.")
     private String description;
+
     private String images;
 }
